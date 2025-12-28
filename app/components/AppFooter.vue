37c8b8272aac0c2aa430ca0746e5db37622536be
otTATto <TATo123SU@gmail.com>
2025-12-29 01:51:54 +0900

    feat: GitHub リポジトリへのリンクボタンを配置

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -2,13 +2,25 @@
   <div class='
     sticky top-[100vh]
     h-[50px] w-full
-    flex items-center justify-center'>
+    flex items-center justify-center gap-x-1'>
     <div class='font-bold text-xs'>
       © <a
         href='https://x.com/0123tato'
         target='_blank'
         class='hover:text-tato-green duration-300 ease-in-out'
       >たと</a>
+    </div>
+    <div>・</div>
+    <div>
+      <a
+        href='https://github.com/otTATto/what-a-year'
+        target='_blank'
+      >
+        <img
+          src='/github-mark.svg'
+          class='w-[15px] hover:fill-tato-green duration-300 ease-in-out'
+        />
+      </a>
     </div>
   </div>
 </template>
